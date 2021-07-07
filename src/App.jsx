1abe62e57c05adf3e57bbb1c465299561793b6f2
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-07-07 19:10:51 +0100

    Add task list layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,24 @@ function App() {
   return (
     <div className="container mt-5">
       <h1 className="text-center">CRUD Simple</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Task List</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <span className="lead">Task Name</span>
+              <button className="btn btn-danger btn-sm float-end mx-2">
+                Delete
+              </button>
+              <button className="btn btn-warning btn-sm float-end">Edit</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Form</h4>
+        </div>
+      </div>
     </div>
   );
 }
