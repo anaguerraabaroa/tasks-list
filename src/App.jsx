187b019f705cbef0086aef8f1e8e031f1db3a205
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-07-07 19:06:49 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">CRUD Simple</h1>
+    </div>
+  );
+}
+
+export default App;
